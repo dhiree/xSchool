ced740019242b139d40fba345ef49d760a0406ac
dheerendra singh <bhandaridheere@gmail.com>
2024-11-22 18:01:46 +0530

    add

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(bodyParser.json());
 const schoolRoutes = require('./routes/schoolRoutes');
 app.use('/api', schoolRoutes);
 
+app.get("/", (req, res) => {
+    res.send("App is running")
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
